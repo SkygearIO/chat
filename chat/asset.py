84c4cea0d3027a858c93b73fd2ac3e7116865ca8
Rick Mak <rick.mak@gmail.com>
2017-10-09 17:12:21 +0800

    pep8 fix

--- chat/asset.py
+++ chat/asset.py
@@ -1,11 +1,6 @@
-import base64
-import hashlib
-import hmac
-from datetime import datetime, timedelta
-
 from skygear.asset import get_signer
 
 
 def sign_asset_url(name):
     signer = get_signer()
-    return signer.sign(name) 
+    return signer.sign(name)
